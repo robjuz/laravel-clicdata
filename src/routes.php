1e44bb8da31d352134965621f0adf61a78ccbc7a
Robert Juzak <robert.juzak@twentyzen.com>
2017-11-24 13:22:47 +0100

    removed DataController

--- src/routes.php
+++ src/routes.php
@@ -7,8 +7,4 @@ Route::group([
 
     Route::get('auth/init', 'OAuthController@init')->name('clicdata.oauth.init');
     Route::get('auth/process', 'OAuthController@process')->name('clicdata.oauth.process');
-
-    Route::get('data', 'DataController@index')->name('clicdata.data.index');
-    Route::get('data/{id}', 'DataController@show')->name('clicdata.data.show');
-
 });
